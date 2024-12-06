d18fea55ec289a363feb3df4863fef0b218d42f4
Tsee90 <179171024+Tsee90@users.noreply.github.com>
2024-12-06 13:11:58 -0500

    adjust style

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { Card } from './components/Card';
 import './App.css';
 
 function App() {
-  return <Card pokemon={'beautifly'}></Card>;
+  return <Card pokemon={'charizard'}></Card>;
 }
 
 export default App;
